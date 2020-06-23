66a9902bfd62a2a2c64295b5d09de2c1a20e546c
Jeremy Durand <jeremy.durand@okta.com>
2020-06-23 17:10:34 +0200

    First commit

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const finale = require('finale-rest');
 const OktaJwtVerifier = require('@okta/jwt-verifier');
 
 const oktaJwtVerifier = new OktaJwtVerifier({
-    clientId: '{clientId}',
+    clientId: '0oagrv82uRw9xGlga4x6',
     issuer: 'https://{yourOktaDomain}/oauth2/default'
 });
 
